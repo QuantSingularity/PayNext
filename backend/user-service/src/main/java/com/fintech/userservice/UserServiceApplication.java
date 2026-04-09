4b2e5230141faaa7507b3898c9ae5768abbbdc3c
Abrar Ahmed <abrarahmedny@gmail.com>
2026-04-09 15:37:49 +0500

    Refactor code and enhance maintainability.

--- backend/user-service/src/main/java/com/fintech/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/fintech/userservice/UserServiceApplication.java
@@ -3,11 +3,16 @@ package com.fintech.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@ComponentScan(basePackages = {
+        "com.fintech.userservice",
+        "com.fintech.common"
+})
 public class UserServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
